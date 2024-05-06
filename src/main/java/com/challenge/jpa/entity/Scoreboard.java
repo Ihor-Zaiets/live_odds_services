be03938a549325eb96b13bda984af659fc9600c7
Ihor Zaiets <dekstroer8202@gmail.com>
2024-05-06 16:19:25 +0200

    get active matches list business logic

--- src/main/java/com/challenge/jpa/entity/Scoreboard.java
+++ src/main/java/com/challenge/jpa/entity/Scoreboard.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Getter
@@ -23,6 +24,13 @@ public class Scoreboard {
         Match match = new Match(homeTeam, awayTeam);
         scoreboardMatchList.add(match);
         return match;
+    }
+
+    public List<Match> getScoreboardMatchList() {
+        return scoreboardMatchList.stream()
+                .sorted(Comparator.comparing(Match::getScore, Comparator.comparingInt((score) -> score.getAwayTeamScore() + score.getHomeTeamScore())).reversed()
+                        .thenComparing(Match::getStartDate))
+                .toList();
     }
 
     public Match updateScore(Match match, Score score) {

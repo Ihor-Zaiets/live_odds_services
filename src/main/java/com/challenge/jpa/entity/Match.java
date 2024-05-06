2b68771cf795dd7d5ff0343ad052c02681009c88
Ihor Zaiets <dekstroer8202@gmail.com>
2024-05-06 14:27:56 +0200

    defined equals method for Match entity

--- src/main/java/com/challenge/jpa/entity/Match.java
+++ src/main/java/com/challenge/jpa/entity/Match.java
@@ -14,4 +14,13 @@ public class Match {
     @NonNull
     private Country awayTeam;
     private Score score = new Score();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Match match)) return false;
+
+        if (getHomeTeam() != match.getHomeTeam()) return false;
+        return getAwayTeam() == match.getAwayTeam();
+    }
 }
